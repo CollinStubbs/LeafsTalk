a8a85d90cdfc00a295dbd0da94a2e642dc3646b1
CollinStubbs <collin.stubbs@uoit.net>
2014-12-13 21:19:39 -0500

    Got the http and xml working

---
+++ src/collin/mobile/leafstalk/GetInfo.java
@@ -0,0 +1,77 @@
+package collin.mobile.leafstalk;
+
+import java.io.BufferedReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class GetInfo extends AsyncTask<Void, Void, Void> {
+	private Document doc;
+	private int rank;
+
+	public int getRank(){
+		
+		return rank;
+	}
+	@Override
+	protected Void doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		
+		
+		try{
+	    	HttpGet uri = new HttpGet("http://app.cgy.nhl.yinzcam.com/V2/Stats/Standings");    
+
+	    	DefaultHttpClient client = new DefaultHttpClient();
+	    	HttpResponse resp = client.execute(uri);
+
+	    	StatusLine status = resp.getStatusLine();
+	    	if (status.getStatusCode() != 200) {
+	    	    Log.d("Hello", "HTTP error, invalid server status code: " + resp.getStatusLine());  
+	    	}
+
+	    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    	DocumentBuilder builder = factory.newDocumentBuilder();
+	    	doc = builder.parse(resp.getEntity().getContent());
+	    	doc.getDocumentElement().normalize();
+	    	resp.getEntity().consumeContent();
+	          
+	    	NodeList nList = doc.getElementsByTagName("Standing");
+	    	 
+	    	
+	     
+	    	for (int temp = 0; temp < nList.getLength(); temp++) {
+	     
+	    		Node nNode = nList.item(temp);
+	     
+	     
+	    		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	     
+	    			Element eElement = (Element) nNode;
+	    			
+	    			if(eElement.getAttribute("TriCode").equals("TOR")){
+	    				rank = Integer.parseInt(eElement.getAttribute("ConfRank"));
+	    			}
+	    		}
+	    			
+	    		
+	    	}
+	    	
+	    	
+	       }catch(Exception e){
+	           Log.e("log_tag", "Error in http connection "+e.toString());
+	       }
+		return null;
+	}
+}
--- src/collin/mobile/leafstalk/MainActivity.java
+++ src/collin/mobile/leafstalk/MainActivity.java
@@ -1,32 +1,62 @@
 package collin.mobile.leafstalk;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+
 import android.support.v7.app.ActionBarActivity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		//IMPLEMENT KABEL FONT
 		TextView pI =(TextView)findViewById(R.id.playoffIntro);
 		TextView pA =(TextView)findViewById(R.id.playoffAns);
 		
 	    Typeface kabe=Typeface.createFromAsset(getAssets(), "fonts/Kabel_Regular.ttf");
 	    Typeface kabeBold=Typeface.createFromAsset(getAssets(), "fonts/Kabel_Bold.ttf");
+	    GetInfo gI = new GetInfo();
+	    gI.execute();
 	    
 	    pA.setTypeface(kabeBold);
 	    pA.setTextSize(40f);
-	    pA.setText("Yes");
+	    if(gI.getRank() <= 8){
+	    	
+	    	pA.setText("Yes");
+	    }else{
+	    	pA.setText("No");
+	    }
 	    
 	    pI.setTypeface(kabe);
 	    pI.setTextSize(24f);
+	    
+	    
+	    
+	    
+	    
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
