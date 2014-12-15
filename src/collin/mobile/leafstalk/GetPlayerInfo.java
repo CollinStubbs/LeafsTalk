29c2c5a793bcf128eedf3c25532eb46b0a709d9a
CollinStubbs <collin.stubbs@uoit.net>
2014-12-15 18:35:49 -0500

    refactoring and comments for get player info

--- src/collin/mobile/leafstalk/GetPlayerInfo.java
+++ src/collin/mobile/leafstalk/GetPlayerInfo.java
@@ -16,14 +16,24 @@ import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
+/*
+ * @name: GetPlayerInfo
+ * @author: Collin Stubbs - 100454604
+ * @desc: This class sends a get request for a json string,
+ *  parses the json string, and returns the top players
+ * 
+ * */
 public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 
 	private ArrayList<String> arr = new ArrayList<String>();
 	private int checker = 0;
 	
+	
+	
 	@Override
 	protected Void doInBackground(Void... params) {
-		// TODO Auto-generated method stub
+		
+		//Http Stuff
 		DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
 		HttpPost httppost = new HttpPost("http://nhlwc.cdnak.neulion.com/fs1/nhl/league/playerstatsline/20142015/2/TOR/iphone/playerstatsline.json");
 		// Depends on your web service
@@ -36,7 +46,7 @@ public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 		    HttpEntity entity = response.getEntity();
 
 		    inputStream = entity.getContent();
-		    // json is UTF-8 by default
+		    
 		    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
 		    StringBuilder sb = new StringBuilder();
 
@@ -50,7 +60,7 @@ public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 		    JSONObject jObject = new JSONObject(result);
 		    JSONArray jArray = jObject.getJSONArray("skaterData");
 		    
-		    
+		    	//take sthe jason info and puts the player info into the Arraylist
 		        try {
 		            JSONObject oneObject = jArray.getJSONObject(0);
 		            JSONObject oneObject1 = jArray.getJSONObject(1);
@@ -63,7 +73,7 @@ public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 		        } catch (JSONException e) {
 		            // Oops
 		        }
-		    
+		        checker = 1;
 		} catch (Exception e) { 
 		    // Oops
 		}
@@ -74,6 +84,7 @@ public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 		return null;
 	}
 
+	//Used to take the players names, add the numbers, and add to arraylist
 	private void fillArray(String one, String two, String three){
 		String oneParts[] = one.split(", ");
 		String twoParts[] = two.split(", ");
@@ -81,17 +92,21 @@ public class GetPlayerInfo extends AsyncTask<Void, Void, Void>{
 		arr.add("1.  "+oneParts[2]+": "+oneParts[4]+"-"+oneParts[5]+"-"+oneParts[6]);
 		arr.add("2.  "+twoParts[2]+": "+twoParts[4]+"-"+twoParts[5]+"-"+twoParts[6]);
 		arr.add("3.  "+threeParts[2]+": "+threeParts[4]+"-"+threeParts[5]+"-"+threeParts[6]);
-		checker = 1;
+		
 	}
+	//gets the first player
 	public String getOne(){
 		return arr.get(0);
 	}
+	//gets the second player
 	public String getTwo(){
 		return arr.get(1);
 	}
+	//gets the third player
 	public String getThree(){
 		return arr.get(2);
 	}
+	//gets the check value -- used to tell if task is complete
 	public int getCheck(){
 		return checker;
 	}
