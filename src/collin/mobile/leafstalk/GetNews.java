43b84658c38fc0a24290381b778f09878891b113
CollinStubbs <collin.stubbs@uoit.net>
2014-12-15 18:42:34 -0500

    refactoring and comments for get news

--- src/collin/mobile/leafstalk/GetNews.java
+++ src/collin/mobile/leafstalk/GetNews.java
@@ -16,7 +16,13 @@ import org.w3c.dom.NodeList;
 
 import android.os.AsyncTask;
 import android.util.Log;
-
+/*
+ * @name: GetNews
+ * @author: Collin Stubbs - 100454604
+ * @desc: This class sends a get request for an xml page,
+ * parses the xml, and stores the data
+ * 
+ * */
 public class GetNews extends AsyncTask<Void, Void, Void> {
 	
 	private ArrayList<String> news = new ArrayList<String>();
@@ -24,39 +30,39 @@ public class GetNews extends AsyncTask<Void, Void, Void> {
 
 	@Override
 	protected Void doInBackground(Void... params) {
-		// TODO Auto-generated method stub
+		
 		try{
+			//all the http stuff
 	    	HttpGet uri = new HttpGet("http://mapleleafs.nhl.com/rss/news.xml");    
 
 	    	DefaultHttpClient client = new DefaultHttpClient();
 	    	HttpResponse resp = client.execute(uri);
 
 	    	StatusLine status = resp.getStatusLine();
+	    	//incase link doesnt work
 	    	if (status.getStatusCode() != 200) {
 	    	    Log.d("Hello", "HTTP error, invalid server status code: " + resp.getStatusLine());  
 	    	}
 
+	    	//for parsing the xml
 	    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	    	DocumentBuilder builder = factory.newDocumentBuilder();
 	    	Document doc = builder.parse(resp.getEntity().getContent());
 	    	doc.getDocumentElement().normalize();
 	    	resp.getEntity().consumeContent();
 	       
+	    	//creates the list of all news items
 	    	NodeList tList = doc.getElementsByTagName("item");
 	    
-	     
+	    	//for every news item, add title and link to the arraylist
 	    	for (int temp = 0; temp < 3; temp++) {
-	     
 	    		Node tNode = tList.item(temp);
-	
 	    		Element tElement = (Element) tNode;
-	    		
 	    		
 	    		news.add(tElement.getElementsByTagName("title").item(0).getTextContent());
 	    		news.add(tElement.getElementsByTagName("link").item(0).getTextContent());	
 	    			
 	    	}
-	    			    	
 	    	
 	       }catch(Exception e){
 	           Log.e("log_tag", "Error in http connection "+e.toString());
@@ -64,25 +70,32 @@ public class GetNews extends AsyncTask<Void, Void, Void> {
 		check = 1;
 		return null;
 	}
+	//returns the check -- used to check if task is done
 	public int getCheck(){
 		
 		return check;
 	}
+	//returns the first link title
 	public String getOneTitle(){
 		return "1.  "+news.get(0);
 	}
+	//returns the first link
 	public String getOneLink(){
 		return news.get(1);
 	}
+	//returns the second link title
 	public String getTwoTitle(){
 		return "2.  "+news.get(2);
 	}
+	//returns the second link
 	public String getTwoLink(){
 		return news.get(3);
 	}
+	//returns the third link title
 	public String getThreeTitle(){
 		return "3.  "+news.get(4);
 	}
+	//returns the third link
 	public String getThreeLink(){
 		return news.get(5);
 	}
